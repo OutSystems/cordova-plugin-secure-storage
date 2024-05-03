189462a718f49473185b1cb70283f06ff1723fb5
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2024-05-03 11:27:30 +0100

    feat: copy biometric prompt info to strings.xml in hook
    References:

--- hooks/android/androidCopyPreferences.js
+++ hooks/android/androidCopyPreferences.js
@@ -2,14 +2,23 @@ const et = require('elementtree');
 const path = require('path');
 const fs = require('fs');
 const { ConfigParser } = require('cordova-common');
+const { DOMParser, XMLSerializer } = require('xmldom');
 
 module.exports = function (context) {
-    var projectRoot = context.opts.cordova.project ? context.opts.cordova.project.root : context.opts.projectRoot;
-    var configXML = path.join(projectRoot, 'config.xml');
-    var configParser = new ConfigParser(configXML);
-    var authenticate = configParser.getGlobalPreference("MigratedKeysAuthentication");    
+    let projectRoot = context.opts.cordova.project ? context.opts.cordova.project.root : context.opts.projectRoot;
+    let configXML = path.join(projectRoot, 'config.xml');
+    let configParser = new ConfigParser(configXML);
+    let authenticate = configParser.getGlobalPreference("MigratedKeysAuthentication");
+    let auth_prompt_title = configParser.getPreference("AuthPromptTitle")
+    let auth_prompt_subtitle = configParser.getPreference("AuthPromptSubtitle")
+    let auth_prompt_negative_button = configParser.getPreference("AuthPromptCancelButton")
+
+    const stringsXmlPath = path.join(projectRoot, 'platforms/android/app/src/main/res/values/strings.xml');
+    const stringsXmlString = fs.readFileSync(stringsXmlPath, 'utf-8');
+    const stringsXmlDoc = parser.parseFromString(stringsXmlString, 'text/xml')
 
     if(authenticate == "true"){
+        /*
         var stringsXmlPath = path.join(projectRoot, 'platforms/android/app/src/main/res/values/strings.xml');
         var stringsXmlContents = fs.readFileSync(stringsXmlPath).toString();
         var etreeStrings = et.parse(stringsXmlContents);
@@ -21,5 +30,45 @@ module.exports = function (context) {
 
         var resultXmlStrings = etreeStrings.write();
         fs.writeFileSync(stringsXmlPath, resultXmlStrings);
+        */
+
+        // insert values in strings.xml
+        
+
+        const booleanElements = stringsXmlDoc.getElementsByTagName('bool');
+        
+        // set text for each <bool> element
+        for (let i = 0; i < booleanElements.length; i++) {
+            const name = booleanElements[i].getAttribute('name');
+            if (name == "migration_auth") {
+                booleanElements[i].textContent = authenticate;
+            }
+        }
+
     }
+
+    const stringElements = stringsXmlDoc.getElementsByTagName('string');
+
+    // set text for each <string> element
+    for (let i = 0; i < stringElements.length; i++) {
+        const name = stringElements[i].getAttribute('name');
+        if (name == "biometric_prompt_title") {
+            stringElements[i].textContent = authenticate;
+        }
+        else if (name == "biometric_prompt_subtitle") {
+            stringElements[i].textContent = notificationDescription;
+        }
+        else if (name == "biometric_prompt_negative_button") {
+            stringElements[i].textContent = notificationDescription;
+        }
+    }
+
+    // serialize the updated XML document back to string
+    const serializer = new XMLSerializer();
+    const updatedXmlString = serializer.serializeToString(stringsXmlDoc);
+
+    // write the updated XML string back to the same file
+    fs.writeFileSync(stringsXmlPath, updatedXmlString, 'utf-8');
+
+
 };
