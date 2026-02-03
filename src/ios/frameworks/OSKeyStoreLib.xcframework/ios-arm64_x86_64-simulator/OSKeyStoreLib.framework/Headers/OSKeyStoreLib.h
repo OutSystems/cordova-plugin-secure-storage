8505bae04b04a755cce069cbe6c7f5935dcabfb6
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-02-03 17:19:01 +0000

    fix(ios): update keystore lib without bitcode

---
+++ src/ios/frameworks/OSKeyStoreLib.xcframework/ios-arm64_x86_64-simulator/OSKeyStoreLib.framework/Headers/OSKeyStoreLib.h
@@ -0,0 +1,12 @@
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OSKeyStoreLib.
+FOUNDATION_EXPORT double OSKeyStoreLibVersionNumber;
+
+//! Project version string for OSKeyStoreLib.
+FOUNDATION_EXPORT const unsigned char OSKeyStoreLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OSKeyStoreLib/PublicHeader.h>
+
+
